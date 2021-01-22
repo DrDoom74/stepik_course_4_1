95cafea6be31b5b4df7719a9812122d1a4838c81
Alexey Klimenko <alexey_klimenko2010@mail.ru>
2021-01-22 12:01:24 +0500

    transfer between pages added

--- test_main_page.py
+++ test_main_page.py
@@ -5,7 +5,7 @@ def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    #page.go_to_login_page()
-    page.should_be_login_link()
-
+    page.go_to_login_page()  # переход на страницу с login
+    login_page = LoginPage(browser, browser.current_url)  #  создание экз. клааса страницы login
+    login_page.should_be_login_page()  # запуск методов проверок url и наличия форм
 
